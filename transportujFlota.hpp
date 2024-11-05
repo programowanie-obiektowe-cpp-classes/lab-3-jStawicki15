83f0e6020b94f5781990a47b396f3ef9d3a579bf
Jakub Stawicki <jakub.stawicki15@gmail.com>
2024-11-05 14:59:24 +0100

    Pierwsza próba kodu

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -1,9 +1,36 @@
 #pragma once
 
 #include "Stocznia.hpp"
+#include <iostream>
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int lŻaglowców = 0;
+    unsigned int remainingTowar = towar;
+    unsigned int towarNaKurs = 0;
+    Stocznia stocznia{};
+
+    if(towar == 0)
+    {
+        std::cout<<"Brak towarow do przewiezienia";
+        return 0;
+    }
+    else
+    {
+        while(remainingTowar>0)
+        {
+            Statek* currentStatek = stocznia();
+            towarNaKurs = currentStatek->transportuj();
+            remainingTowar = remainingTowar - towarNaKurs;
+
+            if(dynamic_cast< Zaglowiec* >(currentStatek) != nullptr)
+                lŻaglowców++;
+            
+            delete currentStatek;
+        }
+
+        std::cout<<"Caly towar przewieziony";
+        return lŻaglowców;
+    }
+
 }
